d4e17f289ed40bc9f0ea83631f5a73bbb1c77440
Andy Mancuso <andymancuso5@gmail.com>
2022-06-28 04:36:21 -0300

    Initial commit

---
+++ componente/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    height: 100px;
+    width 350px;
+    background-color: red;
+  `
+
+const ColIcon = styled.div`
+
+`
+const App = () => {
+
+  return (
+      <Container>
+				
+        <p></p>
+      </Container>
+  )
+}
+
+export default App;
